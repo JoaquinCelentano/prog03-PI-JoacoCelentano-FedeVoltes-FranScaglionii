db33c55c06dbc6a4e39f31e3322a9c29a5331e1d
JoaquinCelentano <jcelentanoalonso@udesa.edu.ar>
2026-04-10 14:39:46 -0300

    mejoras

--- progra3pi/progra3pii/src/components/Home/Home.js
+++ progra3pi/progra3pii/src/components/Home/Home.js
@@ -6,7 +6,7 @@ function Home() {
 
       <h1>UdeSA Movies</h1>
 
-      
+       
       <form className="search-form">
         <input type="text" name="searchData" placeholder="Buscar..." />
         <button type="submit" className="btn btn-success btn-sm">Buscar</button>
@@ -21,7 +21,8 @@ function Home() {
           <div className="cardBody">
             <h5 className="card-title">The Thursday Murder Club</h5>
             <p className="card-text">
-              A group of senior sleuths passionate about solving cold cases get plunged into a real-life murder mystery.
+              A group of senior sleuths passionate about solving cold cases get plunged into
+              a real-life murder mystery in this comic crime caper.
             </p>
             <a href="" className="btn btn-primary">Ver más</a>
             <a href="" className="btn alert-primary">🩶</a>
@@ -33,13 +34,39 @@ function Home() {
           <div className="cardBody">
             <h5 className="card-title">F1</h5>
             <p className="card-text">
-              Racing legend Sonny Hayes returns to Formula 1.
+              Racing legend Sonny Hayes is coaxed out of retirement to lead a struggling
+              Formula 1 team—and mentor a young hotshot driver—while chasing one more chance at glory..
             </p>
             <a href="" className="btn btn-primary">Ver más</a>
             <a href="" className="btn alert-primary">♥️</a>
           </div>
         </article>
 
+        <article className="single-card-movie">
+                <img src="https://image.tmdb.org/t/p/w500/A06yXys3hrCWu8xiNoHCFLTG5SH.jpg" className="card-img-top"
+                    alt="..."/>
+                <div className="cardBody">
+                    <h5 className="card-title">I Know What You Did Last Summer</h5>
+                    <p className="card-text">When five friends inadvertently cause a deadly car accident, they cover up
+                        their involvement and make a pact to keep it a secret rather than face the consequences. A year
+                        later, their past comes back to haunt them and they're forced to confront a horrifying truth:
+                        someone knows what they did last summer…and is hell-bent on revenge.</p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                    <a href="" className="btn alert-primary">♥️</a>
+                </div>
+            </article>
+
+            <article className="single-card-movie">
+                <img src="https://image.tmdb.org/t/p/w500/ombsmhYUqR4qqOLOxAyr5V8hbyv.jpg" className="card-img-top"
+                    alt="..."/>
+                <div className="cardBody">
+                    <h5 className="card-title">Superman</h5>
+                    <p className="card-text">Superman, a journalist in Metropolis, embarks on a journey to reconcile his
+                        Kryptonian heritage with his human upbringing as Clark Kent.</p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                    <a href="" className="btn alert-primary">♥️</a>
+                </div>
+            </article>
       </section>
 
       
@@ -52,12 +79,79 @@ function Home() {
           <div className="cardBody">
             <h5 className="card-title">War of the Worlds</h5>
             <p className="card-text">
-              Analyst tracks threats through surveillance.
+             Will Radford is a top analyst for Homeland Security who tracks potential
+            threats through a mass surveillance program, until one day an attack by an unknown entity leads
+            him to question whether the government is hiding something from him... and from the rest of the
+            world..
             </p>
             <a href="" className="btn btn-primary">Ver más</a>
             <a href="" className="btn alert-primary">🩶</a>
           </div>
         </article>
+
+         <article className="single-card-playing">
+                <img src="https://image.tmdb.org/t/p/w500/9PXZIUsSDh4alB80jheWX4fhZmy.jpg" className="card-img-top"
+                    alt="..."/>
+                <div className="cardBody">
+                    <h5 className="card-title">F1</h5>
+                    <p className="card-text">Racing legend Sonny Hayes is coaxed out of retirement to lead a struggling
+                        Formula 1 team—and mentor a young hotshot driver—while chasing one more chance at glory.</p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                    <a href="" className="btn alert-primary">♥️</a>
+                </div>
+            </article>
+            <article className="single-card-playing">
+                <img src="https://image.tmdb.org/t/p/w500/12Va3oO3oYUdOd75zM57Nx1976a.jpg" className="card-img-top"
+                    alt="..."/>
+                <div className="cardBody">
+                    <h5 className="card-title">Eenie Meanie</h5>
+                    <p className="card-text">A former teenage getaway driver gets dragged back into her unsavory past when a
+                        former employer offers her a chance to save the life of her chronically unreliable ex-boyfriend.
+                    </p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                    <a href="" className="btn alert-primary">🩶</a>
+                </div>
+            </article>
+            <article className="single-card-playing">
+                <img src="https://image.tmdb.org/t/p/w500/1RICxzeoNCAO5NpcRMIgg1XT6fm.jpg" className="card-img-top"
+                    alt="..."/>
+                <div className="cardBody">
+                    <h5 className="card-title">Jurassic World Rebirth</h5>
+                    <p className="card-text">Five years after the events of Jurassic World Dominion, covert operations
+                        expert Zora Bennett is contracted to lead a skilled team on a top-secret mission to secure
+                        genetic material from the world's three most massive dinosaurs. When Zora's operation intersects
+                        with a civilian family whose boating expedition was capsized, they all find themselves stranded
+                        on an island where they come face-to-face with a sinister, shocking discovery that's been hidden
+                        from the world for decades.</p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                    <a href="" className="btn alert-primary">🩶</a>
+                </div>
+            </article>
+            <article className="single-card-playing">
+                <img src="https://image.tmdb.org/t/p/w500/m52XidzKx94bKlToZfEXUnL3pdy.jpg" className="card-img-top"
+                    alt="..."/>
+                <div className="cardBody">
+                    <h5 className="card-title">Together</h5>
+                    <p className="card-text">With a move to the countryside already testing the limits of a couple's
+                        relationship, a supernatural encounter begins an extreme transformation of their love, their
+                        lives, and their flesh.</p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                    <a href="" className="btn alert-primary">🩶</a>
+                </div>
+            </article>
+            <article className="single-card-playing">
+                <img src="https://image.tmdb.org/t/p/w500/A06yXys3hrCWu8xiNoHCFLTG5SH.jpg" className="card-img-top"
+                    alt="..."/>
+                <div className="cardBody">
+                    <h5 className="card-title">I Know What You Did Last Summer</h5>
+                    <p className="card-text">When five friends inadvertently cause a deadly car accident, they cover up
+                        their involvement and make a pact to keep it a secret rather than face the consequences. A year
+                        later, their past comes back to haunt them and they're forced to confront a horrifying truth:
+                        someone knows what they did last summer…and is hell-bent on revenge.</p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                    <a href="" className="btn alert-primary">♥️</a>
+                </div>
+            </article>
 
       </section>
 
