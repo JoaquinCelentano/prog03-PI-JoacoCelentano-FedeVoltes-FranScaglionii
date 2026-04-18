faa5d3e50cc13b60472e0890816d35750f94e413
fvoltess <fedevoltes@gmail.com>
2026-04-18 15:42:38 -0300

    navbar

--- progra3pi/progra3pii/src/components/Navbar/Navbar.js
+++ progra3pi/progra3pii/src/components/Navbar/Navbar.js
@@ -1,33 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Cookies from "universal-cookie";
 
-function Navbar() {
-  return (
-     <nav>
-            <h2>Movies</h2>
+const cookies = new Cookies();
 
-            <ul className="nav nav-tabs my-4">
-                <li className="nav-item">
-                    <Link to="/" className="nav-link">Home</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/movies" className="nav-link">Películas</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/series" className="nav-link">Series</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/favorites" className="nav-link">Favoritas</Link>
-                </li>
-                <li className="nav-item ml-auto">
-                    <Link to="/register" className="nav-link">Registro</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/login" className="nav-link">Login</Link>
-                </li>
-            </ul>
-        </nav>
-  );
+class Navbar extends Component {
+  render() {
+    const isLogged = cookies.get("userLogged");
+
+    let navLinks;
+    if (isLogged) {
+      navLinks = (
+        <li className="nav-item">
+          <Link to="/favorites" className="nav-link">Favoritas</Link>
+        </li>
+      );
+    } else {
+      navLinks = (
+        <React.Fragment>
+          <li className="nav-item ml-auto">
+            <Link to="/register" className="nav-link">Registro</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">Login</Link>
+          </li>
+        </React.Fragment>
+      );
+    }
+
+    return (
+      <nav>
+        <h2>Movies</h2>
+
+        <ul className="nav nav-tabs my-4">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/movies" className="nav-link">Películas</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/series" className="nav-link">Series</Link>
+          </li>
+
+          {navLinks}
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
