373d1a913e3ee4983deb3cc8fe6152029fd4c577
JoaquinCelentano <jcelentanoalonso@udesa.edu.ar>
2026-04-22 14:09:10 -0300

    agregue las importaciones para que funcione

--- progra3pi/progra3pii/src/screens/ResultadosBusqueda/ResultadosBusqueda.js
+++ progra3pi/progra3pii/src/screens/ResultadosBusqueda/ResultadosBusqueda.js
@@ -1,3 +1,7 @@
+import React, { Component } from "react";
+const apiKey ='5aba41484f01b327ba117f875007574f'
+import MovieCard from "../../components/MovieCard/MovieCard";
+
 class ResultadosBusqueda extends Component {
   constructor(props) {
     super(props);
